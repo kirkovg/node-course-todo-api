a8668246a34daf78879b2f4568ea496412c9033c
kirkovg <gjorgjikirkov@gmail.com>
2018-02-24 18:20:33 +0100

    added delete endpoint

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ app.get('/todos/:id', (req, res) => {
 
   Todo.findById(id).then((todo) => {
     if (!todo) {
-      return res.stat(404).send();
+      return res.status(404).send();
     }
 
     return res.send({todo});
@@ -49,6 +49,26 @@ app.get('/todos/:id', (req, res) => {
     res.status(400).send();
   });
 });
+
+app.delete('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    return res.send({todo});
+  }).catch(err => {
+    res.status(400).send();
+  });
+});
+
+
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
