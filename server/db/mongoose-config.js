0ae46f266a204daffc6b05f3197b24b832fa5c64
kirkovg <gjorgjikirkov@gmail.com>
2018-03-01 22:15:11 +0100

    setup test database

--- server/db/mongoose-config.js
+++ server/db/mongoose-config.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/TodoApp';
-
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || url);
+mongoose.connect(process.env.MONGODB_URI);
 
 
 module.exports = {mongoose};
