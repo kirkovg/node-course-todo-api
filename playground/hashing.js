ba8cc1d8056dc09aeb310c87bae49cda695add85
kirkovg <gjorgjikirkov@gmail.com>
2018-03-10 21:39:55 +0100

    jwts and hashing

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,7 +1,37 @@
 const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
 
-const message = 'I am user number 3';
-const hash = SHA256(message).toString();
 
-console.log(`message: ${message}`);
-console.log(`hash: ${hash}`);
+const data = {
+  id: 10
+};
+
+const token = jwt.sign(data, '123abc');
+console.log(token);
+
+const decoded = jwt.verify(token, '123abc');
+console.log(decoded);
+
+// const message = 'I am user number 3';
+// const hash = SHA256(message).toString();
+//
+// console.log(`message: ${message}`);
+// console.log(`hash: ${hash}`);
+
+// const data = {
+//   id: 4
+// };
+//
+// const token = {
+//   data: data,
+//   hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+// };
+//
+//
+// const resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+//
+// if (resultHash === token.hash) {
+//   console.log('data was not changed');
+// } else {
+//   console.log('data was changed');
+// }
